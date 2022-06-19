3fc28f6a24170159fd9ebe364293d8ea96484da8
merajulislamsajeeb <merajulislamsajeeb@gmail.com>
2022-06-20 01:06:25 +0600

    First commit

---
+++ app.js
@@ -0,0 +1,21 @@
+jQuery(document).ready(function(){
+    var slider = tns({
+        container: '.slide-team',
+        items: 4,
+        slideBy: '1',
+        autoplay: false,
+        gutter: 20,
+        controls:false,
+        navPosition:"bottom",
+      });
+
+      var slider = tns({
+        container: '.slide-testimonial',
+        items: 1,
+        slideBy: '1',
+        autoplay: false,
+        gutter: 20,
+        controls:false,
+        navPosition:"bottom",
+      });  
+});
